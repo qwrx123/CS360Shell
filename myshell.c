eed5b92260b0f204211a33d1661d55edf05542e2
Nathaniel Smith <qwrx123@gmail.com>
2025-01-22 12:42:27 -0800

    Implemented basic fork

--- myshell.c
+++ myshell.c
@@ -1,6 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "headers/functions.h"
 
 int main(int argc, char** argv)
 {
-    return 0;
+	int pid;
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Failed to create processes");
+	}
+
+	if (pid == 0)
+	{
+		printf("From child\n");
+	}
+	else if (pid != 0)
+	{
+		waitpid(pid, NULL, 0);
+		printf("From parent\n");
+	}
+	exit(EXIT_SUCCESS);
 }
